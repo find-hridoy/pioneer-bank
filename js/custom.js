cabcfd39eee4ed0d29ad529229d1e324c4d45c24
hridoy546 <mdhridoymia546@gmail.com>
2020-07-23 23:08:50 +0600

    Added second Function

--- js/custom.js
+++ js/custom.js
@@ -9,8 +9,9 @@
  // Deposit button event handler
  const depositBtn = document.getElementById("addDeposit");
  depositBtn.addEventListener("click", function () {
-     const depositAmount = document.getElementById("depositAmount").value;
-     const depositNumber = parseFloat(depositAmount);
+     //  const depositAmount = document.getElementById("depositAmount").value;
+     //  const depositNumber = parseFloat(depositAmount);
+     const depositNumber = getInputNumber("depositAmount");
 
      updateSpanText("currentDeposit", depositNumber);
      updateSpanText("currentBalance", depositNumber);
@@ -18,17 +19,24 @@
      document.getElementById("depositAmount").value = "";
  });
 
- function updateSpanText(id, addInputNumber) {
+ function getInputNumber(id) { // Input Number Fuction
+     const amount = document.getElementById(id).value;
+     const amountNumber = parseFloat(amount);
+     return amountNumber;
+ };
+
+ function updateSpanText(id, addNumber) { // Proccess Function 
      const current = document.getElementById(id).innerText;
      const currentNumber = parseFloat(current);
-     const total = addInputNumber + currentNumber;
+     const total = addNumber + currentNumber;
      document.getElementById(id).innerText = total;
  }
  // Withdraw button event handler
  const withdrawBtn = document.getElementById("addWithdraw");
  withdrawBtn.addEventListener("click", function () {
-     const withdrawAmount = document.getElementById("withdrawAmount").value;
-     const withdrawNumber = parseFloat(withdrawAmount);
+     //  const withdrawAmount = document.getElementById("withdrawAmount").value;
+     //  const withdrawNumber = parseFloat(withdrawAmount);
+     const withdrawNumber = getInputNumber("withdrawAmount");
 
      updateSpanText("currentWithdraw", withdrawNumber);
      updateSpanText("currentBalance", withdrawNumber * -1);
